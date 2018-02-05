b31da7ab5062fe3c60bc1181addf41abf24bd3e7
Dharmedra savaliya <dnksavaliya012@gmail.com>
2018-02-05 10:59:13 +0530

    Update SubmitStartup.bat

--- Startup-India/SubmitStartup.bat
+++ Startup-India/SubmitStartup.bat
@@ -1,18 +1,17 @@
-
-https://ninjaoutreach.com/startup-directories/
-http://www.beingpractical.com/2013/10/12/100-startup-directories-to-submit-your-startup-for-free/
-http://promotehour.com/
-https://inc42.com/startup-submission
-http://knowstartup.com/submit-startup/
-https://www.indianweb2.com/submit-profile/
-https://www.startupbutton.com/submit/
-http://www.thestartupjournal.com/submit/
-https://taalk.com/submit-startup/
-http://submitstartup.com/
-http://www.indianentrepreneur.com/submit-your-startup/
-http://www.mindcron.com/submit-new-startup/
-http://theventures.in/startup-submission/index.htm
-http://startoholics.in/submit-your-startup/
-https://betalist.com/submissions/new
-http://starup.in/
-http://www.indianstartups.com/p/AddStartup1
+start https://ninjaoutreach.com/startup-directories/
+start http://www.beingpractical.com/2013/10/12/100-startup-directories-to-submit-your-startup-for-free/
+start http://promotehour.com/
+start https://inc42.com/startup-submission
+start http://knowstartup.com/submit-startup/
+start https://www.indianweb2.com/submit-profile/
+start https://www.startupbutton.com/submit/
+start http://www.thestartupjournal.com/submit/
+start https://taalk.com/submit-startup/
+start http://submitstartup.com/
+start http://www.indianentrepreneur.com/submit-your-startup/
+start http://www.mindcron.com/submit-new-startup/
+start http://theventures.in/startup-submission/index.htm
+start http://startoholics.in/submit-your-startup/
+start https://betalist.com/submissions/new
+start http://starup.in/
+start http://www.indianstartups.com/p/AddStartup1
